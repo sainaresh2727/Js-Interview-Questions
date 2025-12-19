0b95d6be1d6cc7bef40b487cd32e1ed33c530a85
sainaresh2727 <sainaresh2727@gmail.com>
2025-12-19 08:10:06 +0530

    1st  Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//LOOPING ARRAY ITEMS
+// let Arr1=[1,2,3,4,5,6,7,8,9,10]
+// for (let i=0; i<Arr1.length; i++){
+//     console.log(Arr1[i]);
+// }
+
+//  PRINTING ARRAY IN REVERSE ORDER
+// let Arr2=[1,2,3,4,5,6]
+// for (let i=Arr2.length-1; i>=0; i--){
+//     console.log(Arr2[i]);
+    
+// }
+
+// COUNTING LENGTH WITHOUT USING .LENGTH
+// let Arr3=[1,2,3,4,5]
+// let Count=0;
+
+// for (let i=0; Arr3[i]!==undefined; i++){
+//     Count++
+// }
+// console.log(Count);
