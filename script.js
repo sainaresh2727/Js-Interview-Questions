76dcef40a4d7bc1c9ab9bef30275089f0249f599
sainaresh2727 <sainaresh2727@gmail.com>
2025-12-19 18:40:33 +0530

    2nd Commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+
 //LOOPING ARRAY ITEMS
 // let Arr1=[1,2,3,4,5,6,7,8,9,10]
 // for (let i=0; i<Arr1.length; i++){
@@ -20,3 +22,25 @@
 //     Count++
 // }
 // console.log(Count);
+
+
+
+//COPYING ARRAY INTO ANOTHER ARRAY
+// let Array2=[]
+// for (let i=0; Array[i]!==undefined; i++){
+//     Array2[i]=Array[i]
+// }
+// console.log(Array2);
+
+
+//ADDING NEW ELEMENT 
+// let Array=[1,2,3,4,5]
+// let Element=6
+
+// let i;
+// for (i=0; Array[i]!==undefined; i++);
+
+
+// Array[i]=Element
+
+// console.log(Array);
